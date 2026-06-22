ec9ad387e1dda884ae8f2305c9c36bbc2b6ad221
Matthew Rohde <mrohde213@gmail.com>
2026-06-22 19:09:27 +0900

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        L = 0
+        R = len(s) - 1
+
+        chars = 'addabcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ1234567890'
+        valid = {c for c in chars}
+
+        while L < R:
+            while L < R and s[L] not in valid:
+                L += 1
+            while L < R and s[R] not in valid:
+                R -= 1
+            
+            if s[L].lower() != s[R].lower():
+                return False
+            L += 1
+            R -= 1
+
+        return True

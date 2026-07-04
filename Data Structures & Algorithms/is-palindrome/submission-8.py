c0055ba13dc734c3650a7e98d8a9a0d4ac939a71
Matt Rohde <mrohde213@gmail.com>
2026-07-04 21:33:45 +0900

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        valid = 'abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789'
+
+        left, right = 0, len(s)-1
+
+        while left < right:
+            while not self.alpha_num(s[left]) and left < right:
+                left += 1
+            while not self.alpha_num(s[right]) and left < right:
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+    def alpha_num(self, char):
+        return ('a' <= char <= 'z' or 
+                'A' <= char <= 'Z' or 
+                '0' <= char <= '9')
